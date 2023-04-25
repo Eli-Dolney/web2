08a96fc14d58419bf2152c5c4b623adb86b66782
Eli-Dolney <elid3dev@gmail.com>
2023-04-24 21:33:24 -0500

    merged vue exports and switched root directory

--- site/vue.config.js
+++ site/vue.config.js
@@ -1,10 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/web2/'
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/site/' : '/',
+});
